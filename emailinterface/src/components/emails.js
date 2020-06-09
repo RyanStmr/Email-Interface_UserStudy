714c8323e8c72e9c055aa2ff56e232023744a2ab
RyanStmr <ryan.s@hotmail.de>
2020-06-09 12:27:20 +0200

    dynamic email display, labeling as spam possible, buttons added inside Email-Tab

--- emailinterface/src/components/emails.js
+++ emailinterface/src/components/emails.js
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
-import Mail1 from './mail1';
-import Mail2 from './mail2';
-import Mail3 from './mail3';
+import React, { Component } from "react";
 
 class Emails extends Component {
-    state = { 
-        emails: [
-            { id: 1, },
-            { id: 2, },
-            { id: 3, },
-        ]
-     }
-    render() { 
-        return (<div>
-           { this.state.emails.map(mail1 => < Mail1 key={mail1.id} />,
-           mail2 =><Mail2 key={mail2.id}/>,
-           mail3 =><Mail3 key={mail3.id}/>)}
-        </div>);
-    }
+  state = {
+    emails: [
+      { id: 1, text: "Hello im Email 1" },
+      { id: 2, text: "Hello im Email 2" },
+      { id: 3, text: "Hello im Email 3" },
+    ],
+  };
+  render() {
+    return <div />;
+  }
 }
- 
+
 export default Emails;
