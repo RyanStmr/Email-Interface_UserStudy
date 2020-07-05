f8c6d61065beb1ea1cf46fa7e1b5b2a0dbab68bd
RyanStmr <ryan.s@hotmail.de>
2020-07-05 12:32:12 +0200

    added build

---
+++ emailinterface/build/precache-manifest.ab3f3601521555decda54cedc393aedb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfa3cf3d35368e4a45b3606193436fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25365dc451032f476c92",
+    "url": "/static/css/main.17b1c335.chunk.css"
+  },
+  {
+    "revision": "d6670b1278cec590cd8e",
+    "url": "/static/js/2.70fe1d55.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.70fe1d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25365dc451032f476c92",
+    "url": "/static/js/main.d9489577.chunk.js"
+  },
+  {
+    "revision": "7a11eb87c6d427287b7b",
+    "url": "/static/js/runtime-main.540d9368.js"
+  },
+  {
+    "revision": "5f6cffae5e1283b1a86386af93ac0937",
+    "url": "/static/media/mail1.5f6cffae.jpg"
+  }
+]);
